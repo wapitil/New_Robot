0a49976f1ee59a1641313e0c54968c4f7be9ca40
wapitil <1042629239@qq.com>
2023-10-10 10:51:27 +0100

    更新，识别唤醒有风险

--- Tip/tip.py
+++ Tip/tip.py
@@ -19,15 +19,18 @@ def opening():
     audio_file = "/home/pi/Desktop/New_Robot/Tip/OpenTip.mp3"
     play_audio(audio_file)
 
+
 def begin_show():
     '开始表演，三 二 一'
     audio_file = "/home/pi/Desktop/New_Robot/Tip/BeginShow.mp3"
     play_audio(audio_file)
 
+
 def middle():
     '请 下达指令'
     audio_file = "/home/pi/Desktop/New_Robot/Tip/MiddleTip.mp3"
     play_audio(audio_file)
+
 
 def ending():
     '表演结束，感谢您的观看。'
@@ -35,66 +38,105 @@ def ending():
     play_audio(audio_file)
 
 # 以下为 语音模式所有播报 #
+
+
 def voice_mode():
     '切换为语音模式。'
     audio_file = "/home/pi/Desktop/New_Robot/Tip/VoiceMode.mp3"
     play_audio(audio_file)
+
 
 def lift_lefthand():
     '举 左手'
     audio_file = "/home/pi/Desktop/New_Robot/Tip/LiftLeftHand.mp3"
     play_audio(audio_file)
 
+
 def lift_hands():
     '举 双手'
     audio_file = "/home/pi/Desktop/New_Robot/Tip/LiftHands.mp3"
     play_audio(audio_file)
+
 
 def forword():
     '前进'
     audio_file = "/home/pi/Desktop/New_Robot/Tip/Forword.mp3"
     play_audio(audio_file)
 
+
 def backword():
     '后退'
     audio_file = "/home/pi/Desktop/New_Robot/Tip/Backword.mp3"
     play_audio(audio_file)
+
 
 def move_left():
     '左 移'
     audio_file = "/home/pi/Desktop/New_Robot/Tip/MoveLeft.mp3"
     play_audio(audio_file)
 
+
 def move_right():
     '右 移。'
     audio_file = "/home/pi/Desktop/New_Robot/Tip/MoveRight.mp3"
     play_audio(audio_file)
+
 
 def turn_left():
     '向 左转。'
     audio_file = "/home/pi/Desktop/New_Robot/Tip/TurnLeft.mp3"
     play_audio(audio_file)
 
+
 def turn_right():
     '向 右转。'
     audio_file = "/home/pi/Desktop/New_Robot/Tip/TurnRight.mp3"
     play_audio(audio_file)
+
 
 def leftfootsupport():
     '左脚撑'
     audio_file = "/home/pi/Desktop/New_Robot/Tip/LeftFootSupport.mp3"
     play_audio(audio_file)
 
+
 def rightfootsupport():
     '右脚撑'
     audio_file = "/home/pi/Desktop/New_Robot/Tip/RightFootSupport.mp3"
     play_audio(audio_file)
 
+
+def trapeziusarm():
+    '斜展臂'
+    audio_file = "/home/pi/Desktop/New_Robot/Tip/TrapeziusArm.mp3"
+    play_audio(audio_file)
+
+
+def handsforward():
+    '双手前举'
+    audio_file = "/home/pi/Desktop/New_Robot/Tip/HandsForward.mp3"
+    play_audio(audio_file)
+
+
+def touchhead():
+    '摸头'
+    audio_file = "/home/pi/Desktop/New_Robot/Tip/TouchHead.mp3"
+    play_audio(audio_file)
+
 # 以下为 视觉模式提示词 #
+
+
 def camera_init():
-    # 相机初始化完成，开始检测。
+    '相机初始化完成，开始检测。'
     audio_file = "/home/pi/Desktop/New_Robot/Tip/CameraInit.mp3"
     play_audio(audio_file)
+
+
+def detecting():
+    '正在检测'
+    audio_file = "/home/pi/Desktop/New_Robot/Tip/Detecting.mp3"
+    play_audio(audio_file)
+
 
 def big_font():
     '大字站'
@@ -107,15 +149,18 @@ def lunge():
     audio_file = "/home/pi/Desktop/New_Robot/Tip/Lunge.mp3"
     play_audio(audio_file)
 
+
 def handsup_vision():
     '举双手'
     audio_file = "/home/pi/Desktop/New_Robot/Tip/HandsUp_Vision.mp3"
     play_audio(audio_file)
 
+
 def squat_down():
     '蹲下'
     audio_file = "/home/pi/Desktop/New_Robot/Tip/SquatDown.mp3"
     play_audio(audio_file)
+
 
 def handsonhips():
     '双手叉腰'
@@ -148,6 +193,7 @@ def music_one():
     audio_file = "/home/pi/Desktop/New_Robot/Tip/MusicOne.mp3"
     play_audio(audio_file)
 
+
 def music_two():
     '音乐编号二'
     audio_file = "/home/pi/Desktop/New_Robot/Tip/MusicTwo.mp3"
@@ -171,10 +217,12 @@ def music_five():
     audio_file = "/home/pi/Desktop/New_Robot/Tip/MusicFive.mp3"
     play_audio(audio_file)
 
+
 def music_six():
     '音乐编号六'
     audio_file = "/home/pi/Desktop/New_Robot/Tip/MusicSix.mp3"
     play_audio(audio_file)
+
 
 def song_success():
     '识别成功，请播放 下一首音乐'
@@ -188,9 +236,7 @@ def song_fail():
     play_audio(audio_file)
 
 
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     ''
     begin_show()
     middle()
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from Action.Vision import *
-from Action.Voice import *
-from Action.Dance import *
-from Music.music import music_main
-from Tip.tip import *
-# from Voice.iat_ws_python3 import
 from Voice import iat_ws_python3 as iat
+from Tip.tip import *
+from Music.music import music_main
+from Action.Dance import *
+from Action.Voice import *
+from Action.Vision import *
+# from Human_Keypoints_Detection import vision
 
 
 def VoiceMode():
@@ -45,11 +45,14 @@ def VoiceMode():
             backword()
             # Back_Back()
         elif command == "斜展臂。":
-            TrapeziusArm()
-        elif command == "双手前举":
-            HandsForward()
+            trapeziusarm()
+            # TrapeziusArm()
+        elif command == "双手前举。":
+            handsforward()
+            # HandsForward()
         elif command == "摸头。":
-            TouchHead()
+            touchhead()
+            # TouchHead()
         elif command == "结束表演。":
             ending()
             break
@@ -57,13 +60,13 @@ def VoiceMode():
     pass
 
 
-def VisionMode():
+def VisionMode(v):
+    camera_init()
     flag = [False, False, False, False, False, False, False]  # 初始状态都设置为 False
     end = []
-    num = ''
-    camera_init()
     while True:
-
+        detecting()
+        num = v.get_results()
         if num == [1] and not flag[1]:
             big_font()
             BigFont()
@@ -104,54 +107,52 @@ def VisionMode():
 
 def DanceMode():
     end = []
-    setInitialPosition()
+    # setInitialPosition()
     time.sleep(1)
     song_tip()
     while True:
-        # print(f"end={len(end)}") #　检验有几首歌播放了
         music = music_main()  # 调用music_recognition函数，获取音乐识别结果
-        # music = input() # 无音乐自测
         print(music)
         if music == "Cheap Thrills":
             '第一首音乐'
             music_one()
             begin_show()
-            DanceRequired()
+            # DanceRequired()
             end.append(1)
             song_success()
         elif music == "Visions":
             '第二首音乐'
             music_two()
             begin_show()
-            DanceRequired()
+            # DanceRequired()
             end.append(1)
             song_success()
         elif music == "Whatever It Takes":
             '第三首音乐'
             music_three()
             begin_show()
-            DanceRequired()
+            # DanceRequired()
             end.append(1)
             song_success()
         elif music == "Liberators":
             '第四首音乐'
             music_four()
             begin_show()
-            DanceRequired()
+            # DanceRequired()
             end.append(1)
             song_success()
         elif music == "Pacific Rim (feat. Tom Morello)":
             '第五首音乐'
             music_five()
             begin_show()
-            DanceRequired()
+            # DanceRequired()
             end.append(1)
             song_success()
         elif music == "奇迹再现":
             '第六首音乐'
             music_six()
             begin_show()
-            DanceOwn()
+            # DanceOwn()
             end.append(1)
         elif music is None:
             song_fail()
@@ -162,16 +163,19 @@ def DanceMode():
 
 if __name__ == "__main__":
     ''
+    # v = vision()
+
     while 1:
         opening()
         command = iat.main()
         print(command)
         if command == "视觉模式。":
-            VisionMode()
+            # VisionMode(v)
             '未决定是否应该通过倒数的方式切换模式'
-            DanceMode()
         elif command == "语音模式。":
             VoiceMode()
+        elif command == "舞蹈模式。":  # 识别会有风险
+            DanceMode()
         elif command == "结束表演。":  # 当识别语音为结束时，播报结束提示音并结束整个程序
             ending()
             break
